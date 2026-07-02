5021d77864707a42c357e94c1e54c0598968e188
cezary-stroczynski <cezary.stroczynski@gmail.com>
2026-07-02 12:39:00 +0200

    Add SSM params for SQS and event-bus

--- infrastructure/build_workers.sh
+++ infrastructure/build_workers.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/.."
+export DATABASE_URL=postgres://user:password@localhost/postgres
 cargo lambda build --release --features aws --bin aws_migrator
 cargo lambda build --release --features aws --bin aws_standard_tx_signer
 cargo lambda build --release --features aws --bin aws_standard_tx_sender
